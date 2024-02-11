644d09c07dd03e346aa863e284b68a06425be5a6
Alejandra <informaticotrabajo00@gmail.com>
2024-02-11 11:29:02 -0300

    primer commit

---
+++ assets/js/script.js
@@ -0,0 +1,17 @@
+// Entrada de datos
+const num1 = Number(prompt('Ingrese numero 1: '));
+const num2 = Number(prompt('Ingrese numero 2: '));
+
+// Proceso
+let suma = num1 + num2;
+let resta = num1 - num2;
+let division = num1 / num2;
+let multiplicacion = num1 * num2;
+let modulo = num1 % num2;
+
+// Salida de datos
+console.log("La suma de "+num1+" y "+num2+" es : "+suma);
+console.log("La resta de "+num1+" y "+num2+" es : "+resta);
+console.log("La division de "+num1+" y "+num2+" es : "+division);
+console.log("La multiplicacion de "+num1+" y "+num2+" es : "+multiplicacion);
+console.log("La modulo de "+num1+" y "+num2+" es : "+modulo); 
---
+++ assets/js/scriptcelsius.js
@@ -0,0 +1,10 @@
+// Entrada de datos
+const celsius = Number(prompt('Ingrese grados celsius: '));
+
+// Proceso
+let kelvin = celsius +273.15;
+let fahrenheit =(celsius* (9/5))+32;
+
+// Salida de datos
+alert("Los Kelvin de celsius "+celsius+" es : "+kelvin);
+alert("Los Fahrenheit de celsius "+celsius+" es : "+fahrenheit);
